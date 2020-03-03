06dbc3c2db4ecf69b7c7247d24aefc9519b17c9f
Alc2110 <38523363+Alc2110@users.noreply.github.com>
2020-03-03 22:51:09 +1100

    start implementing invoices

---
+++ POS/View/IInvoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.View
+{
+    public interface IInvoice
+    {
+        void addEntry();
+        void removeEntry();
+    }
+}
---
+++ POS/View/Invoice.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.ObjectModel;
+
+namespace POS.View
+{
+    public abstract class Invoice : IInvoice
+    {
+        public List<Transaction> transactions;
+
+        public void addEntry()
+        {
+            // ask the model to retrieve the transaction object,
+            // based on the supplied information in the parameters
+            Transaction trans = new Transaction();
+            // TODO: implement this
+
+            // add it to the list
+            transactions.Add(trans);
+        }
+
+        public void removeEntry()
+        {
+            // remove the transaction object
+        }
+
+        // save the file in one of the specified formats
+        public abstract void save();
+    }
+
+    public class PDFInvoice : Invoice
+    {
+        public override void save()
+        {
+            // save the PDF file
+        }
+    }
+
+    public class ExcelInvoice : Invoice
+    {
+        public override void save()
+        {
+            // save the spreadsheet file
+        }
+    }
+
+    public class CSVInvoice : Invoice
+    {
+        public override void save()
+        {
+            // save the CSV file
+        }
+    }
+
+    public class PlainTextInvoice : Invoice
+    {
+        public override void save()
+        {
+            // save the text file
+        }
+    }
+
+    public class JSONInvoice : Invoice
+    {
+        public override void save()
+        {
+            // save the JSON file
+        }
+    }
+
+    public class XMLInvoice : Invoice
+    {
+        public override void save()
+        {
+            // save the XML file
+        }
+    }
+}
